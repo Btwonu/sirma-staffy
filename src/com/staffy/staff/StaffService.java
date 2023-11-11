2c1658d8fa472eb9fefb7dbc3d04394e60a9bbbb
vkdelchev <btwonu@gmail.com>
2023-11-11 11:43:17 +0200

    Add json reader

--- src/com/staffy/staff/StaffService.java
+++ src/com/staffy/staff/StaffService.java
@@ -1,4 +1,11 @@
 package com.staffy.staff;
 
+import com.staffy.util.JsonReader;
+
 public class StaffService {
+    JsonReader reader;
+
+    StaffService(JsonReader reader) {
+        this.reader = reader;
+    }
 }
