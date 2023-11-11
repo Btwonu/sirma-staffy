5a42ca5d33c67a45b3e6a3f8caacb8adb2e724f5
vkdelchev <btwonu@gmail.com>
2023-11-11 14:44:55 +0200

    Move json parsing logic to JsonReader

--- src/com/staffy/staff/employee/EmployeeFactory.java
+++ src/com/staffy/staff/employee/EmployeeFactory.java
@@ -8,27 +8,8 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 public class EmployeeFactory {
-    public static Employee create(JSONObject employeeJObject) {
-        int id = employeeJObject.getInt("Id");
-        String name = employeeJObject.getString("Name");
-        Department department = Department.valueOf(employeeJObject.getString("Department").toUpperCase());
-        String role = employeeJObject.getString("Role");
-        double salary = employeeJObject.getDouble("Salary");
-
-        String dateFormat = "yyyy-MM-dd";
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(dateFormat);
-        LocalDate startDate = null;
-        LocalDate endDate = null;
-
-        try {
-            startDate = LocalDate.parse(employeeJObject.getString("StartDate"), formatter);
-            if (employeeJObject.has("EndDate")) {
-                endDate = LocalDate.parse(employeeJObject.getString("EndDate"), formatter);
-            }
-        } catch (DateTimeParseException | JSONException e) {
-            System.out.println("Invalid date format.");
-            e.printStackTrace();
-        }
+    public static Employee create(int id, String name, String departmentString, String role, double salary, LocalDate startDate) {
+        Department department = Department.valueOf(departmentString);
 
         return switch (department) {
             case HR -> new HREmployee(id, name, department, role, salary, startDate);
--- src/com/staffy/util/JsonReader.java
+++ src/com/staffy/util/JsonReader.java
@@ -1,6 +1,8 @@
 package com.staffy.util;
 
+import com.staffy.staff.employee.Department;
 import com.staffy.staff.employee.Employee;
+import com.staffy.staff.employee.EmployeeFactory;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -8,27 +10,51 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.HashMap;
 
 public class JsonReader {
-    public ArrayList<JSONObject> getJsonList(Path filePath) {
-        ArrayList<JSONObject> jsonList = new ArrayList<>();
+    public HashMap<Integer, Employee> getEmployeesMap(Path filePath) {
+        HashMap<Integer, Employee> employeesMap = new HashMap<>();
 
         try {
             String content = Files.readString(filePath);
             JSONArray contentJArray = new JSONArray(content);
 
             for (int i = 0; i < contentJArray.length(); i++) {
-                JSONObject JObject = contentJArray.getJSONObject(i);
+                JSONObject employeeJObject = contentJArray.getJSONObject(i);
 
-                jsonList.add(JObject);
+                int id = employeeJObject.getInt("Id");
+                String name = employeeJObject.getString("Name");
+                String departmentString = employeeJObject.getString("Department").toUpperCase();
+                String role = employeeJObject.getString("Role");
+                double salary = employeeJObject.getDouble("Salary");
+
+                String dateFormat = "yyyy-MM-dd";
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern(dateFormat);
+                LocalDate startDate = null;
+                LocalDate endDate = null;
+
+                try {
+                    startDate = LocalDate.parse(employeeJObject.getString("StartDate"), formatter);
+                    if (employeeJObject.has("EndDate")) {
+                        endDate = LocalDate.parse(employeeJObject.getString("EndDate"), formatter);
+                    }
+                } catch (DateTimeParseException | JSONException e) {
+                    System.out.println("Invalid date format.");
+                    e.printStackTrace();
+                }
+
+                Employee newEmployee = EmployeeFactory.create(id, name, departmentString, role, salary, startDate);
+                employeesMap.put(id, newEmployee);
             }
         } catch (IOException | JSONException e) {
             System.out.println("IO or JSON bad");
             e.printStackTrace();
         }
 
-        return jsonList;
+        return employeesMap;
     }
 }
